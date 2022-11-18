93f471dea9a1cc49a0017a6f09b4c6106b14a7ef
ownavarro1 <112905824+ownavarro1@users.noreply.github.com>
2022-11-17 21:23:27 -0500

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import './App.css';
+import { Link} from "react-router-dom";
 
 
 function App() {
   return (
      <div id="wrapper">
+     
   {/* <!-- Page Wrapper --> */}
         {/* <!-- Sidebar --> */}
         <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             {/* <!-- Sidebar - Brand --> */}
-            <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <Link to="/" className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div className="sidebar-brand-icon rotate-n-15">
                     <i className="fas fa-laugh-wink"></i>
                 </div>
                 <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-            </a>
-
+            </Link>
+           
+            <Link to="/Login" className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div className="sidebar-brand-icon rotate-n-15">
+                    <i className="fas fa-laugh-wink"></i>
+                </div>
+                <div className="sidebar-brand-text mx-3">Login<sup>2</sup></div>
+            </Link>
             {/* <!-- Divider --> */}
             <hr className="sidebar-divider my-0" />
 
@@ -632,38 +640,8 @@ function App() {
 
                         <div className="col-lg-6 mb-4">
 
-                            {/* <!-- Illustrations --> */}
-                            <div className="card shadow mb-4">
-                                <div className="card-header py-3">
-                                    <h6 className="m-0 font-weight-bold text-primary">Illustrations</h6>
-                                </div>
-                                <div className="card-body">
-                                    <div className="text-center">
-                                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width:"25rem"}}
-                                            src="img/undraw_posting_photo.svg" alt="..."/>
-                                    </div>
-                                    <p>Add some quality, svg illustrations to your project courtesy of <a
-                                            target="_blank" rel="nofollow noreferrer" href="https://undraw.co/">unDraw</a>, a
-                                        constantly updated collection of beautiful svg images that you can use
-                                        completely free and without attribution!</p>
-                                    <a target="_blank" rel="nofollow noreferrer" href="https://undraw.co/">Browse Illustrations on
-                                        unDraw &rarr;</a>
-                                </div>
-                            </div>
 
-                            {/* <!-- Approach --> */}
-                            <div className="card shadow mb-4">
-                                <div className="card-header py-3">
-                                    <h6 className="m-0 font-weight-bold text-primary">Development Approach</h6>
-                                </div>
-                                <div className="card-body">
-                                    <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce
-                                        CSS bloat and poor page performance. Custom CSS classes are used to create
-                                        custom components and custom utility classes.</p>
-                                    <p className="mb-0">Before working with this theme, you should become familiar with the
-                                        Bootstrap framework, especially the utility classes.</p>
-                                </div>
-                            </div>
+                       
 
                         </div>
                     </div>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { Login } from './Componentes/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<App />}/>
+        <Route  path='/login' element={<Login />}/>
+      </Routes>
+    </Router>
+    
   </React.StrictMode>
 );
 
